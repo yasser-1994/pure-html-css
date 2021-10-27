1f07122d2d1ebaee3cfb1416d771f31b23993d18
Yasser-1994 <ym3390200@gmail.com>
2021-10-28 00:59:42 +0200

    add js skills

--- js/master.js
+++ js/master.js
@@ -9,3 +9,13 @@ window.onscroll = () => {
     window.scrollTo(0, 0);
   });
 };
+
+let skils = document.querySelector(".our-skills");
+let span = document.querySelectorAll(".progress span");
+window.onscroll = function () {
+  if (window.scrollY >= skils.offsetTop - 100) {
+    span.forEach((span)=>{
+     span.style.width = span.dataset.width;
+    })
+  }
+};
