48455d4f15d0b7f7fbb0a31d3d05334c47f47577
Yasser-1994 <ym3390200@gmail.com>
2021-10-30 17:18:59 +0200

    add statsjs

--- js/master.js
+++ js/master.js
@@ -14,8 +14,45 @@ let skils = document.querySelector(".our-skills");
 let span = document.querySelectorAll(".progress span");
 window.onscroll = function () {
   if (window.scrollY >= skils.offsetTop - 100) {
-    span.forEach((span)=>{
-     span.style.width = span.dataset.width;
-    })
+    span.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
   }
 };
+
+let countDownDate = new Date("Dec 30, 2022 23:59:59").getTime();
+let counter = setInterval(() => {
+  let datNow = new Date().getTime();
+  let dateDiff = countDownDate - datNow;
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+}, 1000);
+
+let nums = document.querySelectorAll(".nums h3");
+let section = document.querySelector(".stats");
+let started = false;
+window.onscroll = function () {
+  if (window.scrollY >= section.offsetTop - 100) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 10 / goal);
+}
+startCount(nums);
